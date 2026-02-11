7cbe7d70976b6de3febb7f4bb2cb1cb7a8c221a2
usr-terio <leoteriod@outlook.com>
2026-02-11 18:05:23 -0500

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-pacman -S --needed base-devel steam gamemode gamescope btrfs-assistant fwupd fwupd-docs sbctl efitools power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar unzip p7zip openh264 firefox ufw blender terminus-font evolution
+pacman -Sy steam gamemode gamescope network-manager-applet fwupd fwupd-docs sbctl efitools power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar unzip ufw gwenview firefox openh264 terminus-font evolution evince file-roller
 
+systemctl enable bluetooth.service
+systemctl enable cups.service
 
-systemctl enable bluetooth
+rm -rf /usr/share/sddm/themes/maya
+/usr/share/applications/assistant.desktop /usr/share/applications/designer.desktop /usr/share/applications/linguist.desktop /usr/share/applications/qdbusviewer.desktop /usr/share/applications/qv4l2.desktop /usr/share/applications/qvidcap.desktop /usr/share/applications/lstopo.desktop /usr/share/applications/org.kde.kwrite.desktop 
 
-pacman -Rs htop
+pacman -Rs htop ark
