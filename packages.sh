86c7f30196332c6a199babfb601feb796e018033
usr-terio <leoteriod@outlook.com>
2026-02-11 17:18:16 -0500

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -3,8 +3,6 @@
 pacman -S --needed base-devel steam gamemode gamescope btrfs-assistant fwupd fwupd-docs sbctl efitools power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar unzip p7zip openh264 firefox ufw blender terminus-font evolution
 
 
-systemctl enable bluetooth.service
-systemctl enable cups.service
-
+systemctl enable bluetooth
 
 pacman -Rs htop
